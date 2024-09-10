c2d8d003af7f597391267fc2c8c21810a86fe1bb
Aditya Nair <adinair0206@gmail.com>
2024-09-10 14:35:06 -0400

    Improved the function

--- loopProblem.cpp
+++ loopProblem.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-//Function that prints out all the prime numbers <=100000
+//Function that prints out all the prime numbers <=10000
 int main(){
 
-    for(int n=2;n<=100000;n++){
+    for(int n=2;n<=100;n++){
         bool unprime = false;
         if (n==2){
             std::cout<<n<<std::endl;
@@ -13,7 +13,7 @@ int main(){
         for(int i=(n-1);i>=2;i--){
             if ((n%i)==0){
                 unprime = true;
-                continue;
+                break;
             }
         }
 
