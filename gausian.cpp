8180d9421beb6c6eca5ad7b8cf8fbdf9e490b010
Aditya Nair <adinair0206@gmail.com>
2024-09-11 16:21:26 -0400

    Added matrix printing code

---
+++ gausian.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    //Make the matrix
+    for (int i=1;i<=8;i++){
+        for (int n=1;n<=(9);n++){
+            std::cout<<i<<n<<" ";
+        }
+        std::cout<<""<<std::endl;
+    }
+
+    
+    return 0;
+};
