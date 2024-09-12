dc52edee9f7e5aee833a34a28f86a894a952891c
Aditya Nair <adinair0206@gmail.com>
2024-09-12 13:46:19 -0400

    Completed gausian using my own logic madafaka

--- gausian.cpp
+++ gausian.cpp
@@ -2,9 +2,19 @@
 
 int main(){
 
+    //Get the numbers for the matrix
+    int row, column;
+    std::cout<<"Enter the row numbers: ";
+    std::cin>>row;
+    column = row+1;
+
     //Make the matrix
-    for (int i=1;i<=8;i++){
-        for (int n=1;n<=(9);n++){
+    for (int i=1;i<=row;i++){
+        for (int n=1;n<=column;n++){
+            if ((n<=(i-1))&&((i-1)>0)){
+                std::cout<<0<<"  ";
+                continue;
+            }
             std::cout<<i<<n<<" ";
         }
         std::cout<<""<<std::endl;
