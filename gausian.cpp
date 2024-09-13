4e5152ca24a24273dc5ef4fbd1d846a2174179a6
Aditya Nair <adinair0206@gmail.com>
2024-09-13 15:01:34 -0400

    Completed gausian

--- gausian.cpp
+++ gausian.cpp
@@ -8,7 +8,6 @@ int main(){
     std::cin>>row;
     column = row+1;
 
-    //Make the matrix
     for (int i=1;i<=row;i++){
         for (int n=1;n<=column;n++){
             if ((n<=(i-1))&&((i-1)>0)){
