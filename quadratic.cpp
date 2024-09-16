c378fe412c6c58aa3c222066f3899d758b0e7c9b
Aditya Nair <adinair0206@gmail.com>
2024-09-15 22:52:01 -0400

    Got inputs

--- quadratic.cpp
+++ quadratic.cpp
@@ -6,6 +6,13 @@ int main();
 int roots();
 
 int main(){
+    double a,b,c;
+    std::cout<<"Enter a: ";
+    std::cin>>a;
+    std::cout<<"Enter b: ";
+    std::cin>>b;
+    std::cout<<"Enter c: ";
+    std::cin>>c;
     
     return 0;
 }
