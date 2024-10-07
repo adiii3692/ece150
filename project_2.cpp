15365d61c7cd86249f3ad9f3deaefdc1269aaf5b
Aditya Nair <adinair0206@gmail.com>
2024-10-07 18:15:49 -0400

    Started project 2

---
+++ project_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cassert>
+
+void pattern(unsigned int n);
+unsigned int log10(unsigned int n);
+unsigned int count(unsigned int n, unsigned int bit);
+unsigned int swap_bytes(unsigned int n, unsigned int b0, unsigned int b1);
+
+void pattern(unsigned int n){
+    if (n==0){
+        std::cout<<"*"<<std::endl;
+        return;
+    }
+    else{
+        
+    }
+}
