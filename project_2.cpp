7da21b594a22df9f8b8e181abefd05cd5ba9922a
adiii3692 <adinair0206@gmail.com>
2024-10-08 00:30:08 -0500

    Completed project 2

--- project_2.cpp
+++ project_2.cpp
@@ -172,31 +172,16 @@ unsigned int swap_bytes(unsigned int n, unsigned int b0, unsigned int b1){
     unsigned int isolater{255};// 11111111
 
     //Isolate the bytes to swap
-    unsigned int byte0 = isolater & (n>>(8*b0));
-    unsigned int byte1 = isolater & (n>>(8*b1));
+    unsigned int byte0 = isolater & (n >> (8 * b0));
+    unsigned int byte1 = isolater & (n >> (8 * b1));
 
-    std::cout<<"Byte 0: "<<byte0<<std::endl;
-    std::cout<<"Byte 1: "<<byte1<<std::endl;
+    //Clear out the spaces where the old bytes used to reside
+    n = n & ~(isolater << (8 * b0));
+    n = n & ~(isolater << (8 * b1));
+
+    //Add the swapped values to the emptied out number
+    n = n | (byte0 << (8 * b1));
+    n = n | (byte1 << (8 * b0)) ;
 
     return n;
 }
-
-
-int main();
-int main(){
-    // pattern(1);
-    // unsigned int n = 4294967295;
-    // std::cout<<log10(n)<<std::endl;
-    // std::cout<<count(n,1)<<std::endl;
-    // unsigned int n = 255;
-
-    // unsigned int check{n << 8};
-    // std::cout<<check<<std::endl;
-
-    unsigned int n = 56128356;
-    swap_bytes(n,0,3);
-
-    // std::cout<<(255 & (1093>>0))<<std::endl;
-    // return 0;
-}
-// g++ -o project_2 project_2.cpp
