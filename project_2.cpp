b576318d68806b9034c71ba4ef431f4ae6be5842
adiii3692 <adinair0206@gmail.com>
2024-10-07 19:33:51 -0500

    Completed pattern of project_2

--- project_2.cpp
+++ project_2.cpp
@@ -5,13 +5,71 @@ void pattern(unsigned int n);
 unsigned int log10(unsigned int n);
 unsigned int count(unsigned int n, unsigned int bit);
 unsigned int swap_bytes(unsigned int n, unsigned int b0, unsigned int b1);
+int main();
 
+/**
+ * @brief Prints out a pattern based on an integer paramater
+ * 
+ * @param n - The integer parameter which defines the pattern.
+ * @return Returns nothing.
+ */
 void pattern(unsigned int n){
+
+    //Check the edge case of n=0
     if (n==0){
         std::cout<<"*"<<std::endl;
         return;
     }
     else{
-        
+        //Find the number of rows and columns based on the parameter n
+        int outer = (2*n)+1;
+
+        //Print the top-down pattern
+        for(int rows=outer;rows>=1;rows -= 2){
+            int split = (outer-rows)/2;
+            int stars = split + rows;
+
+            for(int column{1};column<=outer;column++){
+                if (column<=split){
+                    std::cout<<" ";
+                }
+                else if ((column>split)&&(column<=stars)){
+                    std::cout<<"*";
+                }
+                else{
+                    std::cout<<" ";
+                }
+            }
+
+            std::cout<<std::endl;
+        }
+
+        //Print the bottom-up pattern now
+        for(int rows{3};rows<=outer;rows+=2)
+        {
+            int split = (outer-rows)/2;
+            int stars = split + rows;
+
+            for(int column{1};column<=outer;column++){
+                if (column<=split){
+                    std::cout<<" ";
+                }
+                else if ((column>split)&&(column<=stars)){
+                    std::cout<<"*";
+                }
+                else{
+                    std::cout<<" ";
+                }
+            }
+
+            std::cout<<std::endl;
+        }
     }
 }
+
+
+
+int main(){
+    pattern(7);
+}
+// g++ -o project_2 project_2.cpp
