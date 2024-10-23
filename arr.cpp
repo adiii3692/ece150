2c6fb3846a7e600eb4093d1ea034b2c6bf9bf309
adiii3692 <adinair0206@gmail.com>
2024-10-23 14:15:57 -0500

    Arrays

---
+++ arr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main();
+
+int main(){
+    int temperatures[10]{};
+    double values[5]{};
+
+    for(int i{0};i<5;i++){
+        std::cout<<values[i]<<std::endl;
+    }
+}
