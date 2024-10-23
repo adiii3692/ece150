2284c7bb22963810e33166a6be51388af80c5785
adiii3692 <adinair0206@gmail.com>
2024-10-23 14:22:16 -0500

    Must pass capacity as a separate arguement for functions

--- arr.cpp
+++ arr.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
 int main();
+double average(double values[], int capacity);
 
 int main(){
-    int temperatures[10]{};
-    double values[5]{};
+    double values[5]{34,6.6257,18,678.9,627.945};
 
-    for(int i{0};i<5;i++){
-        std::cout<<values[i]<<std::endl;
+    std::cout<<"The average is: "<<average(values,5)<<std::endl;
+}
+
+double average(double values[], int capacity){
+    double sum = 0;
+    for(int i{0};i<capacity;i++){
+        sum += values[i];
     }
+    
+    double avg = sum/capacity;
+
+    return avg;
 }
